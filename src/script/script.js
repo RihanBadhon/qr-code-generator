8d8ac35dd8aa5d560e778a29be3fe1cfeda686e0
Rihan Badhon <rihanmiahbadon@gmail.com>
2024-11-22 19:09:55 -0800

    assignment done

---
+++ src/script/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let imgBox = document.getElementById("image-box");
+let qrImage = document.getElementById("qr-image");
+let qrText = document.getElementById("qr-text");
+
+
+function generateQR(){
+  if(qrText.value.length > 0){
+    // Using The qr code generator API below
+   qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="
+   + qrText.value;
+   qrText.value = "";
+  } 
+}
+
